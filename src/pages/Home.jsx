ce1bff4af69a432638d5f8302411f053c94a39ff
ri_ming <86471461+12ing@users.noreply.github.com>
2025-10-29 11:44:07 +0900

    [feat] #1 - Hero 화면 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react"
 import "./Home.css"
+import Rogo1 from "../assets/Rogo1.svg"
+import ArrowDown from "../assets/arrow_down.svg"
 
 function Home() {
   return (
     <div className="page-container">
-      <h1>Home Page</h1>
+      {/* 메인 화면 */}
+      <div id="hero-section" className="page-container">
+        <h1>
+          I'm ready.
+          <br />
+          <span>
+            Already
+            <img src={Rogo1} alt="Rim Rogo" className="Rogo1" />
+          </span>
+        </h1>
+
+        <img src={ArrowDown} alt="ArrowDown" className="ArrowDown" />
+      </div>
+
+      {/* About me 화면 */}
+      <div id="AboutMe" className="page-container">
+        <p>페이지 하단</p>
+      </div>
     </div>
   )
 }
