bfdbbad7e322bc19f1e5acf5ca95d8cbbb09e1ec
ri_ming <86471461+12ing@users.noreply.github.com>
2025-10-30 19:26:43 +0900

    [feat] #5 - AboutMeValues 컴포넌트 구현

---
+++ src/components/AboutMevalues.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import "./AboutMeValues.css"
+import AboutMeData from "../data/projects.json"
+import InsightIcon from "../assets/insight.svg"
+import ActionIcon from "../assets/action.svg"
+import RepeatIcon from "../assets/repeat.svg"
+
+const iconMap = {
+  1: InsightIcon,
+  2: ActionIcon,
+  3: RepeatIcon,
+}
+
+function AboutMeValues({ valueId }) {
+  const valueData = AboutMeData.AboutMeValues.find((value) => value.id === valueId)
+
+  if (!valueData) {
+    return null
+  }
+
+  return (
+    <section className="aboutme-values-section">
+      <div className="aboutme-values-container">
+        {/* 왼쪽: 아이콘 + 타이틀 */}
+        <div className="aboutme-values-left">
+          <div className="aboutme-values-icon-area">
+            <div className="aboutme-values-icon-wrapper">
+              <img src={iconMap[valueData.id]} alt={valueData.title} className="aboutme-values-icon" />
+              <h2 className="aboutme-values-title">{valueData.title}</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* 오른쪽: 텍스트 내용 */}
+        <div className="aboutme-values-right">
+          <span className="aboutme-values-tag">{valueData.tag}</span>
+          <h3 className="aboutme-values-subtitle">{valueData.subtitle}</h3>
+          <p className="aboutme-values-description">{valueData.description}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutMeValues
